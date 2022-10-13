a507fb7141b18aeb90a2c778fe1409b19a568a79
lawrence.li <tiantonl@andrew.cmu.edu>
2022-10-13 12:04:14 -0400

    first commit

---
+++ StarWarsProtocols.playground/Contents.swift
@@ -0,0 +1,149 @@
+/*:
+ # Star Wars Protocols
+ 
+ Below are six structs -- three for Jedi ("Master ...") and three for Sith ("Darth ...").  As you can see comparing the structs, there are some powers which are *specific to the Jedi*, some that are *specific to the Sith*, and some that are *common to any Force user*, whether Jedi or Sith.  Using protocols and extensions as appropriate, refactor this set of structs so that each struct is smaller and less verbose, but retains all of the functionality listed in the handout.
+*/
+//struct MasterYoda {
+//  func lightSaberSkill() -> Int { return 8 }
+//  func telekinesis() -> Int { return 9 }
+//  func prescience() -> Int { return 5 }
+//  func introspection() -> String { return "Search your feelings." }
+//  func mindControl() -> String { return "Do or do not." }
+//}
+
+//struct MasterObiWan {
+//  func lightSaberSkill() -> Int { return 8 }
+//  func telekinesis() -> Int { return 5 }
+//  func prescience() -> Int { return 5 }
+//  func introspection() -> String { return "Search your feelings." }
+//  func mindControl() -> String { return "These are not the droids you're looking for." }
+//}
+
+//struct MasterQuiGon {
+//  func lightSaberSkill() -> Int { return 8 }
+//  func telekinesis() -> Int { return 5 }
+//  func prescience() -> Int { return 5 }
+//  func mindControl() -> String { return "Republic credits will do." }
+//  func introspection() -> String { return "Feel, don’t think. Trust your instincts." }
+//}
+
+//struct DarthVader {
+//  func lightSaberSkill() -> Int { return 9 }
+//  func telekinesis() -> Int { return 8 }
+//  func prescience() -> Int { return 5 }
+//  func forceChoke() -> Bool { return true }
+//  func forceLightning() -> Bool { return false }
+//}
+//
+//struct DarthSidious {
+//  func lightSaberSkill() -> Int { return 8 }
+//  func telekinesis() -> Int { return 7 }
+//  func prescience() -> Int { return 6 }
+//  func forceChoke() -> Bool { return true }
+//  func forceLightning() -> Bool { return true }
+//}
+//
+//struct DarthTyrannous {
+//  func lightSaberSkill() -> Int { return 8 }
+//  func telekinesis() -> Int { return 5 }
+//  func prescience() -> Int { return 5 }
+//  func forceChoke() -> Bool { return false }
+//  func forceLightning() -> Bool { return true }
+//}
+
+
+// Lawrence Li (tiantonl)
+/*:
+ Place your refactored structs below:
+ */
+protocol ForcePowers {
+  func lightSaberSkill() -> Int
+  func telekinesis() -> Int
+  func prescience() -> Int
+}
+
+extension ForcePowers {
+  func lightSaberSkill() -> Int {
+    return 8
+  }
+  
+  func telekinesis() -> Int {
+    return 5
+  }
+  
+  func prescience() -> Int {
+    return 5
+  }
+}
+
+protocol JediPowers {
+  func mindControl() -> String
+  func introspection() -> String
+}
+
+extension JediPowers {
+  func introspection() -> String {
+    return "Search your feelings."
+  }
+}
+
+protocol DarthPowers {
+  func forceChoke() -> Bool
+  func forceLightning() -> Bool
+}
+
+extension DarthPowers {
+  func forceChoke() -> Bool {
+    return true
+  }
+  func forceLightning() -> Bool {
+    return true
+  }
+}
+
+protocol Darth: ForcePowers, DarthPowers {}
+
+protocol Jedi:ForcePowers, JediPowers {}
+
+
+struct MasterYoda: Jedi {
+  func mindControl() -> String {
+    return "Do or do not."
+  }
+  
+  func telekinesis() -> Int {
+    return 9
+  }
+}
+
+struct MasterObiWan: Jedi {
+  func mindControl() -> String {
+    return "These are not the droids you're looking for."
+  }
+}
+
+struct MasterQuiGon:Jedi {
+  func mindControl() -> String {
+    return "Republic credits will do."
+  }
+}
+
+struct DarthVader: Darth {
+  func lightSaberSkill() -> Int { return 9 }
+  func telekinesis() -> Int { return 8 }
+  func forceLightning() -> Bool { return false }
+}
+
+struct DarthSidious: Darth {
+  func telekinesis() -> Int { return 7}
+  func prescience() -> Int { return 6}
+}
+
+struct DarthTyrannous: Darth {
+  func forceChoke() -> Bool { return false }
+}
+
+
+
+
+
